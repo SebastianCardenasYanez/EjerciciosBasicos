7ef8eba0457885a77567ccc5a08974405eb708ab
SebastianCardenasYanez <160754840+SebastianCardenasYanez@users.noreply.github.com>
2024-03-01 14:11:08 -0500

    5 ejercicios

--- NumeroInvertido.py
+++ NumeroInvertido.py
@@ -1,2 +1,8 @@
-num1 = int(input("ingrese un numero de 3 digitos: "))
-
+def invertir_entero():
+    n = int(input("Ingrese un número entero: "))
+    n = str(n)  # Convertir el entero a una cadena
+    n = n[::-1]  # Invertir la cadena
+    n = int(n)  # Convertir la cadena invertida a un entero
+    print (n)
+    return n
+invertir_entero()
---
+++ horaFutura.py
@@ -0,0 +1,5 @@
+t = input("ingrese la hora actual : ")
+h = int(input("ingrese una cantidad de horas : "))
+
+horas =  
+print(f"en {h} horas, el reloj marcara las {horas}")
