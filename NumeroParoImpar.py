372218f8a74f0739310216317c3094b125ddba97
SebastianCardenasYanez <160754840+SebastianCardenasYanez@users.noreply.github.com>
2024-03-06 14:49:07 -0500

    ejercicio numero par o impar

---
+++ NumeroParoImpar.py
@@ -0,0 +1,7 @@
+num = int(input("Ingrese un numero : "))
+if num == 0 :
+    print(f"El numero {num} no es par ni impar")
+elif num % 2 == 0 :
+    print(f"El numero {num} es par")
+else :
+    print (f"El numero {num} es impar")
