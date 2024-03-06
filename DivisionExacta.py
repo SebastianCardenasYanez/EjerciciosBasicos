e126434ef1c392f33d9669c51b26c68300264781
SebastianCardenasYanez <160754840+SebastianCardenasYanez@users.noreply.github.com>
2024-03-06 15:21:54 -0500

    quede en medio de la division

---
+++ DivisionExacta.py
@@ -0,0 +1,7 @@
+dividendo = int(input("Ingrese el dividendo : "))
+divisor = int(input("Ingrese el divisor : "))
+if dividendo % divisor == 0 :
+    resto = dividendo % divisor == 0
+    print(f"la division es exacta el resto es {resto}")
+    
+ 
